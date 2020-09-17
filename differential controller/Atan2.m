3adfcc5183549044f22b52892be44b0ff6c704c1
Ed Amutete <53301147+ed-learner@users.noreply.github.com>
2020-09-17 23:58:34 +0200

    edit atan2

--- differential controller/Atan2.m
+++ differential controller/Atan2.m
@@ -1,20 +1,29 @@
 %% Atan2c function
 
-function out = Atan2ctest(in1,in2,in3)
+function out = Atan2(in1,in2,in3)
 
 i=2;
-phi(i-1)=in3;
-PHI(i)= atan2(in1,in2);
-PHI(i-1)=atan2(sin(phi(i-1)),cos(phi(i-1)));
-D_PHI(i)= PHI(i)-PHI(i-1);
+phi(i-1) = in3;
+PHI(i) = atan2(in1,in2);
+PHI(i-1) = atan2(sin(phi(i-1)),cos(phi(i-1)));
+D_PHI(i) = PHI(i)-PHI(i-1);
+
 if D_PHI(i)>pi
-    d_phi(i)= D_PHI(i) -2*pi;
+
+    d_phi(i) = D_PHI(i) - 2*pi;
+    
 elseif D_PHI(i)<-pi
-    d_phi(i)= D_PHI(i) +2*pi;
+
+    d_phi(i)= D_PHI(i) + 2*pi;
+    
 else
+
     d_phi(i)= D_PHI(i);
-%PHI(i)= PHI(i-1) + d_phi(i);
+    
 end
-PHI(i)= phi(i-1) + d_phi(i); %MK
-out=PHI(i);
+
+PHI(i) = phi(i-1) + d_phi(i);
+
+out = PHI(i);
+
 end
